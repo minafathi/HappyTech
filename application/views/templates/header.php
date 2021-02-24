255cead4b71bb0bd8dc99c841b3835dc922fda5b
Mina Fathi <mina.fathi2012@gmail.com>
2021-02-24 11:32:05 +0000

    Add files via upload

---
+++ application/views/templates/header.php
@@ -0,0 +1,43 @@
+
+<html>
+    <header>
+        <title>HappyTech</title>
+        <link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.min.css">
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="<?php echo base_url(); ?>assets/js/app.js"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </header>
+    <body>
+        <nav class="navbar navbar-expand-lg">
+            <a class="navbar-brand" href="<?php echo base_url(); ?>">HappyTech</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                <li class="nav-item home" >
+                    <a class="nav-link" href="<?php echo base_url(); ?>">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item dashboard">
+                    <a class="nav-link" href="<?php echo base_url(); ?>/dashboard">Dashboard</a>
+                </li>
+                <li class="nav-item pending">
+                    <a class="nav-link" href="<?php echo base_url(); ?>/pending">Pending Reviews</a>
+                </li>
+                <li class="nav-item reviewed">
+                    <a class="nav-link" href="<?php echo base_url(); ?>/reviewed">Reviewed</a>
+                </li>
+                <li class="nav-item app">
+                    <a class="nav-link" href="<?php echo base_url(); ?>/app">Application</a>
+                </li>
+                <li class="nav-item feedback">
+                    <a class="nav-link" href="<?php echo base_url(); ?>/feedback">Feedback</a>
+                </li>
+                <li class="nav-item login">
+                    <a class="nav-link login" href="<?php echo base_url(); ?>/feedback">Login</a>
+                </li>
+                </ul>
+            </div>
+        </nav>
+    
---
+++ assets/js/app.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+                
+    if($(".home  a").attr("href")==window.location.href){
+        $('.nav-item').removeClass('active');
+        $(".home").attr("class","nav-item home active");
+    }
+    if($(".dashboard  a").attr("href")==window.location.href){
+        $('.nav-item').removeClass('active');
+        $(".dashboard").attr("class","nav-item dashboard active");
+    }
+    if($(".pending  a").attr("href")==window.location.href){
+        $('.nav-item').removeClass('active');
+        $(".pending").attr("class","nav-item pending active");
+    }
+    if($(".reviewed  a").attr("href")==window.location.href){
+        $('.nav-item').removeClass('active');
+        $(".reviewed").attr("class","nav-item reviewed active");
+    }
+    if($(".app  a").attr("href")==window.location.href){
+        $('.nav-item').removeClass('active');
+        $(".app").attr("class","nav-item app active");
+    }
+    if($(".feedback  a").attr("href")==window.location.href){
+        $('.nav-item').removeClass('active');
+        $(".feedback").attr("class","nav-item feedback active");
+    }
+        
+        
+    
+});

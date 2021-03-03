cadb03a5c6cd05e811c114191212c0fd83021bdd
Mina Fathi <mina.fathi2012@gmail.com>
2021-03-03 14:08:51 +0000

    Update feedback.php

--- application/views/pages/feedback.php
+++ application/views/pages/feedback.php
@@ -1,1 +1,32 @@
-This is feedback
+<div class="feedbackback">
+    <img class="backimg feed" src="<?php echo base_url() ?>assets/images/6c1299c2a5d7fb27788d6df7d4a5dc3b.jpg" />
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 tmplbox1">
+                <strong><p><?php echo"Applicant     William Smit"; ?></p></strong> 
+                <form action="" method="GET">
+                    <strong><?php echo "Reviewer   "; ?></strong><input type="text" name="reviewer" placeholder="Enter Your Name" />
+                    <div class="box"></div>
+                    <strong class="cttitle">Create Template for CV</strong>
+                    <div class="yellowbox">
+
+                        <p><strong><?php echo "Title  "; ?></strong><input type="text" name="reviewer" placeholder="Enter Template Title" /></p>
+                        <p><strong><?php echo "Comment 1  "; ?></strong><input type="text" name="reviewer" placeholder="Enter a Comment" /></p>
+                        <p><strong><?php echo "Comment 2  "; ?></strong><input type="text" name="reviewer" placeholder="Enter a Comment" /></p>
+                        <p><strong><?php echo "Comment 3  "; ?></strong><input type="text" name="reviewer" placeholder="Enter a Comment" /></p>
+                        <p><Strong><h2>+</h2></Strong></p>
+                        Type:</br>
+                        <input type="radio" name="gender" value="female">Accept
+                        <br>
+                        <input type="radio" name="gender" value="male">Reject
+                    </div>
+                </form>
+
+            </div>
+            
+            <div class="col-md-4 tmplbox2">
+            <strong><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></strong>
+            </div>
+        </div>
+    </div>
+</div>

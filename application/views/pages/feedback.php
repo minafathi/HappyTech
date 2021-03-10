ca85d2b45c95ee29393d8796b4715223a0e2e499
Mina Fathi <mina.fathi2012@gmail.com>
2021-03-10 13:48:44 +0000

    Update feedback.php

--- application/views/pages/feedback.php
+++ application/views/pages/feedback.php
@@ -3,30 +3,88 @@
     <div class="container">
         <div class="row">
             <div class="col-md-4 tmplbox1">
-                <strong><p><?php echo"Applicant     William Smit"; ?></p></strong> 
-                <form action="" method="GET">
-                    <strong><?php echo "Reviewer   "; ?></strong><input type="text" name="reviewer" placeholder="Enter Your Name" />
+                
+                <form action="<?php echo base_url(); ?>template/create_tmpl" method="post">
                     <div class="box"></div>
                     <strong class="cttitle">Create Template for CV</strong>
                     <div class="yellowbox">
 
-                        <p><strong><?php echo "Title  "; ?></strong><input type="text" name="reviewer" placeholder="Enter Template Title" /></p>
-                        <p><strong><?php echo "Comment 1  "; ?></strong><input type="text" name="reviewer" placeholder="Enter a Comment" /></p>
-                        <p><strong><?php echo "Comment 2  "; ?></strong><input type="text" name="reviewer" placeholder="Enter a Comment" /></p>
-                        <p><strong><?php echo "Comment 3  "; ?></strong><input type="text" name="reviewer" placeholder="Enter a Comment" /></p>
-                        <p><Strong><h2>+</h2></Strong></p>
+                        <p><strong>Title</strong><input type="text" name="title" placeholder="Enter Template Title" /></p>
+                        <div class='cmbox'>
+                            <p><strong>Comment 1</strong><input type="text" name="comment[]" placeholder="Enter a Comment" /></p>
+                            <p><strong>Comment 2</strong><input type="text" name="comment[]" placeholder="Enter a Comment" /></p>
+                            <p><strong>Comment 3</strong><input type="text" name="comment[]" placeholder="Enter a Comment" /></p>
+                        </div>
+                        <p><Strong><h2 class='addcm'>+</h2></Strong></p>
                         Type:</br>
-                        <input type="radio" name="gender" value="female">Accept
+                        <input type="radio" name="type" value="accept">Accept
                         <br>
-                        <input type="radio" name="gender" value="male">Reject
+                        <input type="radio" name="type" value="reject">Reject
+                        <br/><br/>
+                        <button type='submit' class='btn btn-success' value='Create'>Create</button>
                     </div>
                 </form>
 
             </div>
             
             <div class="col-md-4 tmplbox2">
-            <strong><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></strong>
+                <br /><br />
+                <form method='post' action=''>
+                    <div class="commentbox">
+                        <table>
+                            <tr>
+                                <th>Id</th>
+                                <th>Comment</th>
+                            </tr>
+                            <tr>
+                                <td><input type='radio' value='1'>1</td>
+                                <td>Some Comment</td>
+                            </tr>
+                            <tr>
+                                <td><input type='radio' value='2'>2</td>
+                                <td>Some Comment</td>
+                            </tr>
+                            <tr>
+                                <td><input type='radio' value='3'>3</td>
+                                <td>Some Comment</td>
+                            </tr>
+                            <tr>
+                                <td><strong>Add to:</strong><input type='text' name='id' class='idinp'></td>
+                                <td><input type='text' name='comment'></td>
+                            </tr>
+                        </table>
+                        <div class="row crow">
+                            <div class='com-md-4 cmb'><button type='submit' value='Delete' class='btn btn-danger'>Delete</button></div>
+                            <div class='com-md-4 cmb'><button type='submit' value='Edit' class='btn btn-primary'>Edit</button></div>
+                            <div class='com-md-4 cmb'><button type='submit' value='Select' class='btn btn-success'>Select</button></div>
+                        </div>
+                    </div>
+                </form>
             </div>
+        </div>
+    </div>
+    <br/><br/><br/>
+    <div class="container">
+        <div class="bluebox">
+            <form method='post' action=''>
+                <strong><p>Applicant     William Smit</p></strong> 
+                <br /><br />
+                <strong>Reviewer    </strong><input type="text" name="reviewer" placeholder="Enter Your Name" />
+                <br /><br />
+                <strong>Header    </strong><input type='text' name='header' placeholder='Enter header for feedback' />
+                <br /><br />
+                <input type='radio' value='now' name='sendingop'>Send Now
+                <br />
+                <input type='radio' value='now' name='sendingop'>Send Later
+                <br />
+                <strong>Date:    </strong><input class='fdate' type='text' name='date'>
+                <br /><br />
+                <strong>Free Comment :</strong>
+                <br />
+                <textarea name='fcomment'></textarea>
+                <br/><br/>
+                <button type='submit' class='btn btn-success subform' value='Submit'>Submit</button>
+            </form>        
         </div>
     </div>
 </div>

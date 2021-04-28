452c5a8046aad7d2fce899867d907c1587a89121
Baisil Geevarghese <pgbaisil@gmail.com>
2021-04-28 13:45:07 +0100

    Update PendingModel.php

--- application/models/PendingModel.php
+++ application/models/PendingModel.php
@@ -1,11 +1,21 @@
 <?php
 class PendingModel extends CI_Model
 {
-	/*View*/
+	/*Display*/
 	function pending()
 	{
-		$query=$this->db->query("select * from login");
+		$query=$this->db->query("select * from application");
 		return $query->result();
 	}
-
+	function pendingById($id)
+	{
+		$query=$this->db->query("select * from form where id='".$id.”’”);
+		return $query->result();
+	}
+	/*Update*/
+	function edit_review($fullname,$email,$email,$address,$degree,$work,)
+	{
+		$query=$this->db->query("update form SET email='$email' where id='$id’”);
+	}
+	
 }

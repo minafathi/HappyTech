c3f2f1ff2f1cf044b283fbdf06828bdc53b71d7d
Baisil Geevarghese <pgbaisil@gmail.com>
2021-04-28 13:45:11 +0100

    Update Pending.php

--- application/controllers/Pending.php
+++ application/controllers/Pending.php
@@ -12,12 +12,31 @@ class Pending extends CI_Controller
 		/*load Model*/
 		$this->load->model('PendingModel');
 	}
-	/*Display*/
-	public function pending()
+	public function pendingdis()
 	{
-		$result['data']=$this->PendingModel->pending();
-		$this->load->view('pending',$result);
+		$result['data']=$this->Hello_model->pending();
+		$this->load->view('pending	',$result);
 	}
+	public function editpending()
+	{
+		$id=$this->input->get('id');
+		$result['data']=$this->PendingModel->editreviewById($id);
+		$this->load->view('edit_review',$result);
 
+		if($this->input->post('edit'))
+		{
+			$first_name=$this->input->post('id');
+			$first_name=$this->input->post('fullname');
+			$first_name=$this->input->post('email');
+			$first_name=$this->input->post('address');
+			$first_name=$this->input->post('fieldofstudy');
+			$first_name=$this->input->post('degree');
+			$email=$this->input->post('work');
+			$this->PendingModel->edit_review($id,$fullname,$email,$address,$fieldofstudy,$degree);
+			echo "Data updated successfully !”;
+		}
+	}
+	
 }
 ?>
+
